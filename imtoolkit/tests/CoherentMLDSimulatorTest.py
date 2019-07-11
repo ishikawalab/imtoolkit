bbb035ddda8d067f42b281e04c6ad93e1ec89c52
X1CARBON\ishikawa <naoki@ishikawa.cc>
2019-07-11 15:55:41 +0900

    Improved threshold

--- imtoolkit/tests/CoherentMLDSimulatorTest.py
+++ imtoolkit/tests/CoherentMLDSimulatorTest.py
@@ -19,7 +19,7 @@ class CoherentMLDSimulatorTest(unittest.TestCase):
         sim = CoherentMLDSimulator(code.codes, channel)
         ret = sim.simulateBERReference(params, output = False)
         retnorm = np.mean(np.power(np.abs(np.log10(ret["ber"]) - truth), 2))
-        self.assertLessEqual(retnorm, 1e-3)
+        self.assertLessEqual(retnorm, 1e-2)
     
     def test_BERParallel_SM(self):
         truth = np.log10(np.array([3.39290432141913567143e-01,2.28799154240169139163e-01,1.16562376687524657526e-01,4.63410907317818518414e-02,1.58151968369606328590e-02,5.27619894476021120827e-03,1.64459967108006571007e-03]))
@@ -29,7 +29,7 @@ class CoherentMLDSimulatorTest(unittest.TestCase):
         sim = CoherentMLDSimulator(code.codes, channel)
         ret = sim.simulateBERParallel(params, output = False)
         retnorm = np.mean(np.power(np.abs(np.log10(ret["ber"]) - truth), 2))
-        self.assertLessEqual(retnorm, 1e-3)
+        self.assertLessEqual(retnorm, 1e-2)
 
     def test_AMIReference_SM(self):
         truth = np.array([1.42818014515766478212e-02,4.46896133972787268362e-02,1.35020118355007934241e-01,3.74152908820060137174e-01,8.88430916560169148255e-01,1.63202684937455133607e+00,2.32075268455922545385e+00,2.73556303528255551072e+00,2.90887984047466385817e+00])
@@ -39,7 +39,7 @@ class CoherentMLDSimulatorTest(unittest.TestCase):
         sim = CoherentMLDSimulator(code.codes, channel)
         ret = sim.simulateAMIReference(params, output = False)
         retnorm = np.mean(np.power(np.abs(ret["ami"] - truth), 2))
-        self.assertLessEqual(retnorm, 1e-3)
+        self.assertLessEqual(retnorm, 1e-2)
     
     def test_AMIParallel_SM(self):
         truth = np.array([1.42818014515766478212e-02,4.46896133972787268362e-02,1.35020118355007934241e-01,3.74152908820060137174e-01,8.88430916560169148255e-01,1.63202684937455133607e+00,2.32075268455922545385e+00,2.73556303528255551072e+00,2.90887984047466385817e+00])
@@ -49,7 +49,7 @@ class CoherentMLDSimulatorTest(unittest.TestCase):
         sim = CoherentMLDSimulator(code.codes, channel)
         ret = sim.simulateAMIParallel(params, output = False)
         retnorm = np.mean(np.power(np.abs(ret["ami"] - truth), 2))
-        self.assertLessEqual(retnorm, 1e-3)
+        self.assertLessEqual(retnorm, 1e-2)
 
     def test_BERReference_OSTBC(self):
         truth = np.log10(np.array([2.72212300000000018141e-01,1.46606500000000000705e-01,4.03784000000000017905e-02,3.73150000000000000785e-03]))
@@ -59,7 +59,7 @@ class CoherentMLDSimulatorTest(unittest.TestCase):
         sim = CoherentMLDSimulator(code.codes, channel)
         ret = sim.simulateBERReference(params, output = False)
         retnorm = np.mean(np.power(np.abs(np.log10(ret["ber"]) - truth), 2))
-        self.assertLessEqual(retnorm, 1e-3)
+        self.assertLessEqual(retnorm, 1e-2)
 
     def test_BERParallel_OSTBC(self):
         truth = np.log10(np.array([2.72212300000000018141e-01,1.46606500000000000705e-01,4.03784000000000017905e-02,3.73150000000000000785e-03]))
@@ -69,7 +69,7 @@ class CoherentMLDSimulatorTest(unittest.TestCase):
         sim = CoherentMLDSimulator(code.codes, channel)
         ret = sim.simulateBERParallel(params, output = False)
         retnorm = np.mean(np.power(np.abs(np.log10(ret["ber"]) - truth), 2))
-        self.assertLessEqual(retnorm, 1e-3)
+        self.assertLessEqual(retnorm, 1e-2)
 
     def test_AMIReference_OSTBC(self):
         truth = np.array([2.34009305866733186008e-01,5.31335636295124702499e-01,8.56159232876912645871e-01,9.85823618846433324947e-01,9.99561665572359148157e-01,9.99989963240521695376e-01,9.99999993834903122547e-01])
@@ -79,7 +79,7 @@ class CoherentMLDSimulatorTest(unittest.TestCase):
         sim = CoherentMLDSimulator(code.codes, channel)
         ret = sim.simulateAMIReference(params, output = False)
         retnorm = np.mean(np.power(np.abs(ret["ami"] - truth), 2))
-        self.assertLessEqual(retnorm, 1e-3)
+        self.assertLessEqual(retnorm, 1e-2)
 
     def test_AMIParallel_OSTBC(self):
         truth = np.array([2.34009305866733186008e-01,5.31335636295124702499e-01,8.56159232876912645871e-01,9.85823618846433324947e-01,9.99561665572359148157e-01,9.99989963240521695376e-01,9.99999993834903122547e-01])
@@ -89,7 +89,7 @@ class CoherentMLDSimulatorTest(unittest.TestCase):
         sim = CoherentMLDSimulator(code.codes, channel)
         ret = sim.simulateAMIParallel(params, output = False)
         retnorm = np.mean(np.power(np.abs(ret["ami"] - truth), 2))
-        self.assertLessEqual(retnorm, 1e-3)
+        self.assertLessEqual(retnorm, 1e-2)
         
 if __name__ == '__main__':
     unittest.main()
